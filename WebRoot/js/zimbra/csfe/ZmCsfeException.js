13159f758738a53f2997c902426b8ec4a4a9c75e
Greg Solovyev <greg@zimbra.com>
2006-05-02 13:28:50 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=7439 bug 7439 add volume.NO_SUCH_PATH error throw Server exception instead of JavaScript error if the exception is a Csfe exception
    Copied from Perforce
     Change: 24924

--- WebRoot/js/zimbra/csfe/ZmCsfeException.js
+++ WebRoot/js/zimbra/csfe/ZmCsfeException.js
@@ -117,6 +117,7 @@ ZmCsfeException.define("MAIL_SEND_ABORTED_ADDRESS_FAILURE", "mail.SEND_ABORTED_A
 ZmCsfeException.define("MAIL_SEND_FAILURE", "mail.SEND_FAILURE", ZMsg.mailSendFailure);
 ZmCsfeException.define("MAIL_TOO_MANY_CONTACTS", "mail.TOO_MANY_CONTACTS", ZMsg.errorTooManyContacts);
 
+ZmCsfeException.define("VOLUME_NO_SUCH_PATH", "volume.NO_SUCH_PATH");
 // structured data keys
 ZmCsfeException.MAIL_SEND_ADDRESS_FAILURE_INVALID = "invalid";
 ZmCsfeException.MAIL_SEND_ADDRESS_FAILURE_UNSENT = "unsent";
