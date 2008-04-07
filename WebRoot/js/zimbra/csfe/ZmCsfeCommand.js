1d200cb1a29f5aab794bb0c916603625fb0f0d93
Unknown Perforce User <ccao>
2008-04-07 15:42:11 -0500

    bug: 22049 use "public users"
    http://bugzilla.zimbra.com/show_bug.cgi?id=22049
    
    Copied from Perforce
     Change: 91957

--- WebRoot/js/zimbra/csfe/ZmCsfeCommand.js
+++ WebRoot/js/zimbra/csfe/ZmCsfeCommand.js
@@ -76,7 +76,7 @@ function() {
 
 ZmCsfeCommand.setSessionId =
 function(sessionId) {
-	var id = (sessionId instanceof Array) ? sessionId[0].id : sessionId.id;
+	var id = (sessionId instanceof Array) ? sessionId[0].id : sessionId;
 	ZmCsfeCommand._sessionId = parseInt(id);
 };
 
@@ -197,13 +197,10 @@ function(params) {
 	}
 	if (params.noSession) {
 		context.nosession = {};
-	} else {
-		var sessionId = ZmCsfeCommand.getSessionId();
-		if (sessionId) {
-			context.session = {_content:sessionId, id:sessionId};
-		} else {
-			context.session = {};
-		}
+	}
+	var sessionId = ZmCsfeCommand.getSessionId();
+	if (sessionId) {
+		context.sessionId = {_content:sessionId, id:sessionId};
 	}
 	if (params.targetServer) {
 		context.targetServer = {_content:params.targetServer};
@@ -225,8 +222,8 @@ function(params) {
 	}
 	
 	// Tell server what kind of response we want
-	if (params.useXml) {
-		context.format = {type:"xml"};
+	if (!params.useXml) {
+		context.format = {type:"js"};
 	}
 
 	params.methodNameStr = ZmCsfeCommand.getMethodName(params.jsonObj);
@@ -271,12 +268,11 @@ function(params) {
 	
 		if (params.noSession) {
 			soapDoc.set("nosession", null, context);
-		} else {
-			var sessionId = ZmCsfeCommand.getSessionId();
-			var si = soapDoc.set("session", null, context);
-			if (sessionId) {
-				si.setAttribute("id", sessionId);
-			}
+		}
+		var sessionId = ZmCsfeCommand.getSessionId();
+		if (sessionId) {
+			var si = soapDoc.set("sessionId", null, context);
+			si.setAttribute("id", sessionId);
 		}
 		if (params.targetServer) {
 			soapDoc.set("targetServer", params.targetServer, context);
@@ -480,8 +476,8 @@ function(response, params) {
 		}
 	}
 
-	if (obj.Header && obj.Header.context && obj.Header.context.session) {
-		ZmCsfeCommand.setSessionId(obj.Header.context.session);
+	if (obj.Header && obj.Header.context && obj.Header.context.sessionId) {
+		ZmCsfeCommand.setSessionId(obj.Header.context.sessionId);
 	}
 
 	return params.asyncMode ? result : obj;
