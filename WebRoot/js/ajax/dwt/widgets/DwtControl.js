2cc925e618639900c343e9794925d460a149b87c
Conrad Damon <cdamon@zimbra.com>
2008-05-28 13:37:28 -0500

    Integrate change 99825 from FRANKLIN to main
    -------------
    Bug: 20689 - Need to support new MS Office 2007 format extensions
    Add new content types to MIME table
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=20689
    
    Copied from Perforce
     Change: 99826

--- WebRoot/js/ajax/dwt/widgets/DwtControl.js
+++ WebRoot/js/ajax/dwt/widgets/DwtControl.js
@@ -2353,9 +2353,7 @@ function(ev) {
 						obj._setDragProxyState(true);
 						obj.__dropAllowed = true;
 						destDwtObj._dragEnter(mouseEv);
-					}
-					else
-					{
+					} else {
 						obj._setDragProxyState(false);
 						obj.__dropAllowed = false;
 					}
@@ -2431,6 +2429,25 @@ function(ev) {
 				obj._destroyDragProxy(obj._dndProxy);
 				obj._dragging = DwtControl._NO_DRAG;
 			} else {
+				DwtControl.__badDrop(obj, mouseEv);
+			}
+			mouseEv._stopPropagation = true;
+			mouseEv._returnValue = false;
+			mouseEv.setToDhtmlEvent(ev);
+			return false;
+		}
+	}
+};
+
+/**
+ * Handles a bad DND drop operation by showing an animation of the icon flying
+ * back to its origin.
+ * 
+ * @param obj		[DwtControl]	control that underlies drag operation
+ * @param mouseEv	[DwtMouseEvent]	mouse event
+ */
+DwtControl.__badDrop =
+function(obj, mouseEv) {
 	obj._dragSource._cancelDrag();
 	// The following code sets up the drop effect for when an
 	// item is dropped onto an invalid target. Basically the
@@ -2445,13 +2462,6 @@ function(ev) {
 	var m = (obj.__dragEndY - obj.__dragStartY) / (obj.__dragEndX - obj.__dragStartX);
 	obj.__badDropAction.args = [m, obj.__dragStartY - (m * obj.__dragStartX), (obj.__dragStartX - obj.__dragEndX < 0) ? -1 : 1];
 	AjxTimedAction.scheduleAction(obj.__badDropAction, 0);
-			}
-			mouseEv._stopPropagation = true;
-			mouseEv._returnValue = false;
-			mouseEv.setToDhtmlEvent(ev);
-			return false;
-		}
-	}
 };
 
 /**
