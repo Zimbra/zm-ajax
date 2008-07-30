e33462b144f3fa0af68dc45314fcbeea4b301ce2
Unknown Perforce User <dcomfort>
2008-07-30 18:50:04 -0500

    Bug: 30384 Integrate from main. Tweak size of date fields in quick add appointment dialog.
    http://bugzilla.zimbra.com/show_bug.cgi?id=30384
    
    Copied from Perforce
     Change: 109068

--- WebRoot/js/ajax/dwt/widgets/DwtControl.js
+++ WebRoot/js/ajax/dwt/widgets/DwtControl.js
@@ -2378,7 +2378,9 @@ function(ev) {
 						obj._setDragProxyState(true);
 						obj.__dropAllowed = true;
 						destDwtObj._dragEnter(mouseEv);
-					} else {
+					}
+					else
+					{
 						obj._setDragProxyState(false);
 						obj.__dropAllowed = false;
 					}
@@ -2454,25 +2456,6 @@ function(ev) {
 				obj._destroyDragProxy(obj._dndProxy);
 				obj._dragging = DwtControl._NO_DRAG;
 			} else {
-				DwtControl.__badDrop(obj, mouseEv);
-			}
-			mouseEv._stopPropagation = true;
-			mouseEv._returnValue = false;
-			mouseEv.setToDhtmlEvent(ev);
-			return false;
-		}
-	}
-};
-
-/**
- * Handles a bad DND drop operation by showing an animation of the icon flying
- * back to its origin.
- * 
- * @param obj		[DwtControl]	control that underlies drag operation
- * @param mouseEv	[DwtMouseEvent]	mouse event
- */
-DwtControl.__badDrop =
-function(obj, mouseEv) {
 	obj._dragSource._cancelDrag();
 	// The following code sets up the drop effect for when an
 	// item is dropped onto an invalid target. Basically the
@@ -2487,6 +2470,13 @@ function(obj, mouseEv) {
 	var m = (obj.__dragEndY - obj.__dragStartY) / (obj.__dragEndX - obj.__dragStartX);
 	obj.__badDropAction.args = [m, obj.__dragStartY - (m * obj.__dragStartX), (obj.__dragStartX - obj.__dragEndX < 0) ? -1 : 1];
 	AjxTimedAction.scheduleAction(obj.__badDropAction, 0);
+			}
+			mouseEv._stopPropagation = true;
+			mouseEv._returnValue = false;
+			mouseEv.setToDhtmlEvent(ev);
+			return false;
+		}
+	}
 };
 
 /**
