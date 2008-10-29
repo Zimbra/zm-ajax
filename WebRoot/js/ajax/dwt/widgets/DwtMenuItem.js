40cb79dbccecd805b5d606e9ef1b9470b128c7a7
Unknown Perforce User <rpatil>
2008-10-28 22:46:53 -0500

    bug: 30460 Integrating to FRANKLIN.
    http://bugzilla.zimbra.com/show_bug.cgi?id=30460
    
    Copied from Perforce
     Change: 120811

--- WebRoot/js/ajax/dwt/widgets/DwtMenuItem.js
+++ WebRoot/js/ajax/dwt/widgets/DwtMenuItem.js
@@ -165,13 +165,6 @@ function(delay) {
 	this._hoverDelay = delay;
 };
 
-DwtMenuItem.prototype.setShortcut =
-function(shortcut) {
-	if (shortcut && this._dropDownEl) {
-		this._dropDownEl.innerHTML = shortcut;
-	}
-};
-
 //
 // Protected methods
 //
