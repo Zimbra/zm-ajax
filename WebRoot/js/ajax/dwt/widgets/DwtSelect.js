de18de3d68de8eb0df65d5f3fc7b2851499adaa0
Unknown Perforce User <pshah>
2009-02-18 17:31:32 -0600

    Integrate change 146342 from FRANKLIN to main
    -------------
    bug: 33870 - zdesktop hangs on loading
    --
    Add missing try/catch block around isRegisteredProtocolHandler.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=33870
    
    Copied from Perforce
     Change: 146345

--- WebRoot/js/ajax/dwt/widgets/DwtSelect.js
+++ WebRoot/js/ajax/dwt/widgets/DwtSelect.js
@@ -123,6 +123,7 @@ function(element) {
  */
 DwtSelect.prototype.addOption =
 function(option, selected, value) {
+	if (!option) { return; }
 	var opt = null;
 	var val = null;
 	if (typeof(option) == 'string') {
@@ -202,6 +203,24 @@ function(value, newValue) {
 
 	// Register listener to create new menu.
 	this.setMenu(this._menuCallback, true);
+};
+
+/**
+ * Enables or disables an option.
+ *
+ * @param value		{object} 	value 		the value of the option to enable/disable
+ * @param enabled	{Boolean}	enabled 	true to enable the option
+ */
+DwtSelect.prototype.enableOption =
+function(value, enabled) {
+	var option = this.getOptionWithValue(value);
+	if (option.enabled != enabled) {
+		option.enabled = enabled;
+		var item = option.getItem();
+		if (item) {
+			item.setEnabled(enabled);
+		}
+	}
 };
 
 DwtSelect.prototype.clearOptions =
@@ -404,6 +423,7 @@ DwtSelect.prototype._createMenu = function() {
 		if (text) {
 			mi.setText(AjxStringUtil.htmlEncode(text));
 		}
+		mi.setEnabled(option.enabled);
 
 		mi.addSelectionListener(new AjxListener(this, this._handleOptionSelection));
 		mi._optionIndex = i;
@@ -447,10 +467,8 @@ function(option) {
  		if (displayValue) {
  			this.setText(AjxStringUtil.htmlEncode(displayValue));
  		}
- 		if (image) {
- 			this.setImage(image);
- 		}
-		this._selectedValue = option._value;
+ 		this.setImage(image);
+ 		this._selectedValue = option._value;
 		this._selectedOption = option;
 	}
     this._updateSelection(option);
@@ -525,6 +543,7 @@ DwtSelectOption = function(value, selected, displayValue, owner, optionalDOMId, 
 	this._selectedValue = selectedValue;
 
 	this._internalObjectId = DwtSelect._assignId(this);
+	this.enabled = true;
 }
 
 DwtSelectOption.prototype.setItem = 
