f23fbd4df27000f046be0505812876fb701c5508
Unknown Perforce User <mizhang>
2011-03-30 21:58:41 -0500

    Integrate change 285477 from zimbra to zimbra
    -------------
    Bug 48679
    Revert change for this bug
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=48679
    
    Copied from Perforce
     Change: 285478

--- WebRoot/js/ajax/util/AjxStringUtil.js
+++ WebRoot/js/ajax/util/AjxStringUtil.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -1582,7 +1582,9 @@ function(obj, recurse, showFuncs, omit) {
 			stopRecursion = !recurse;
 			var keys = new Array();
 			for (var i in obj) {
-				keys.push(i);
+                if (obj.hasOwnProperty(i)) {
+                    keys.push(i);
+                }
 			}
 
 			if (isArray) {
