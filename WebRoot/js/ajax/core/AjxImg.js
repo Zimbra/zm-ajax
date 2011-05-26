a521f64d22e1f149f3147b60fc57434c5bfac06f
Conrad Damon <cdamon@zimbra.com>
2011-05-25 19:04:56 -0500

    Integrate change 314624 from main to HELIX
    -------------
    Bug: 60310 - Shift-Left Arrow to select bubble causes Chrome to crash
    Don't try to handle key events that have modifiers
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=60310
    
    Copied from Perforce
     Change: 314625

--- WebRoot/js/ajax/core/AjxImg.js
+++ WebRoot/js/ajax/core/AjxImg.js
@@ -229,22 +229,20 @@ function(imageName, styleStr, attrStr, wrapInTable, _disabled) {
 /**
  * Gets the "image" as an HTML string.
  *
- * @param imageName		     the image you want to render
- * @param imageStyleStr      optional style info (for example, "display:inline")
- * @param attrStr		     optional attributes (for example, "id=X748")
- * @param label			     the text that follows this image
- * @param containerClassName class to use instead of the default inlineIcon class
- * @return	{string}	     the image string
+ * @param imageName		the image you want to render
+ * @param styleStr		optional style info (for example, "display:inline")
+ * @param attrStr		optional attributes (for example, "id=X748")
+ * @param label			the text that follows this image
+ * @return	{string}	the image string
  */
 AjxImg.getImageSpanHtml =
-function(imageName, imageStyleStr, attrStr, label, containerClassName) {
-    containerClassName = containerClassName || "inlineIcon";
+function(imageName, styleStr, attrStr, label) {
+	var className = AjxImg.getClassForImage(imageName);
+
 	var html = [
         "<span style='white-space:nowrap'>",
-        "<span class='",
-        containerClassName,
-        "'>",
-        AjxImg.getImageHtml(imageName, imageStyleStr, attrStr),
+        "<span class='inlineIcon'>",
+        AjxImg.getImageHtml(imageName, styleStr, attrStr),
         (label || ""),
         "</span>",
         "</span>"
