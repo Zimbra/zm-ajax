a9c36ee7324fbcc9faf77d4809260dbd76c73c8d
Unknown Perforce User <jwagner>
2011-08-04 12:52:52 -0500

    Integrate change 324169 from HELIX to main
    -------------
    Bug: 62155 Occasional bad rendering of tags
    The HTML5 canvas object requires the image object referenced in canvas.drawImage to be fully downloaded to render correctly.  Added a check for maskImg and overlayImage to see if there are completely downloaded (img.complete). If not, added onload method to the mask & overlay image to handle drawing the image.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=62155
    
    Copied from Perforce
     Change: 324172

--- WebRoot/js/ajax/dwt/core/Dwt.js
+++ WebRoot/js/ajax/dwt/core/Dwt.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -1333,7 +1333,6 @@ function(htmlEl,html){
  */
 Dwt.setFavIcon =
 function(iconURL) {
-	return; // Unsupported on IE. Too CPU heavy on FF. Now seems too CPU heavy on Chrome too. Let's disable this.
 
 	// Look for an existing fav icon to modify.
 	var favIcon = null;
