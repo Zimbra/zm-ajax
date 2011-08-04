bb5e1e774ef928a8f713c0b18ed8e90c3f8d293c
Unknown Perforce User <jwagner>
2011-08-04 12:48:56 -0500

    Bug: 62155 Occasional bad rendering of tags The HTML5 canvas object requires the image object referenced in canvas.drawImage to be fully downloaded to render correctly.  Added a check for maskImg and overlayImage to see if there are completely downloaded (img.complete). If not, added onload method to the mask & overlay image to handle drawing the image.
    http://bugzilla.zimbra.com/show_bug.cgi?id=62155
    
    Copied from Perforce
     Change: 324169

--- WebRoot/js/ajax/debug/AjxDebug.js
+++ WebRoot/js/ajax/debug/AjxDebug.js
@@ -97,6 +97,7 @@ AjxDebug.SCROLL			= "scroll"; 	// bug 55775
 AjxDebug.BAD_JSON		= "bad_json"; 	// bug 57066
 AjxDebug.FILTER         = "filter";     // bug 59158
 AjxDebug.REMINDER       = "reminder";   // bug 60692
+AjxDebug.TAG_ICON       = "tagIcon";    // bug 62155
 
 AjxDebug.BUFFER_MAX[AjxDebug.DEFAULT_TYPE]	= 0;	// this one can get big due to object dumps
 AjxDebug.BUFFER_MAX[AjxDebug.RPC]			= 200;
@@ -108,6 +109,7 @@ AjxDebug.BUFFER_MAX[AjxDebug.SCROLL]		= 100;
 AjxDebug.BUFFER_MAX[AjxDebug.BAD_JSON]		= 200;
 AjxDebug.BUFFER_MAX[AjxDebug.FILTER]        = 100;
 AjxDebug.BUFFER_MAX[AjxDebug.REMINDER]      = 200;
+AjxDebug.BUFFER_MAX[AjxDebug.TAG_ICON]      = 200;
 
 AjxDebug.MAX_OUT = 25000; // max length capable of outputting an XML msg
 
