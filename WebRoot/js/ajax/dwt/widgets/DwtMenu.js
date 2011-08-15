847316f294deea9f8b49568858e3c31a4561d469
Eran Yarkon <eyarkon@zimbra.com>
2011-08-15 15:26:15 -0500

    Integrate change 325716 from HELIX to main
    -------------
    Bug 63354
    mailto link causes newlines to disappear
    replace newline with <br> of extraBodyText (comes from the
    EmailTooltipZimlet.prototype._composeListener which is what's called when clicking on an email "mailto" link)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=63354
    
    Copied from Perforce
     Change: 325717

--- WebRoot/js/ajax/dwt/widgets/DwtMenu.js
+++ WebRoot/js/ajax/dwt/widgets/DwtMenu.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -293,8 +293,10 @@ function(key, id) {
 	var items = this.getItems();
 	for (var i = 0; i < items.length; i++) {
 		var itemId = items[i].getData(key);
-		if (itemId == id)
+		if (itemId == id) {
+			items[i].index = i; //needed in some caller
 			return items[i];
+		}
 	}
 	return null;
 };
@@ -365,7 +367,7 @@ function(msec, x, y, kbGenerated) {
 };
 
 DwtMenu.prototype.popdown =
-function(msec) {
+function(msec, ev) {
 	if (this._style == DwtMenu.BAR_STYLE) return;
 
 	if (this._popupActionId != -1) {
@@ -375,7 +377,7 @@ function(msec) {
 		if (!this._isPoppedUp || this._popdownActionId != -1)
 			return;
 		if (msec == null || msec == 0)
-			this._doPopdown();
+			this._doPopdown(ev);
 		else
 			this._popdownActionId = AjxTimedAction.scheduleAction(this._popdownAction, msec);
 	}
@@ -894,7 +896,14 @@ function(which) {
 		currItem = this._children.get(which);
 	}
 	// While the current item is not enabled or is a separator, try another
-	while (currItem && (currItem.isStyle(DwtMenuItem.SEPARATOR_STYLE) || !currItem.getEnabled() || !currItem.getVisible())) {
+	while (currItem) {
+		if (!currItem.isStyle) { //this is not a DwtMenuItem. e.g. it's DwtFolderChooser
+			currItem.focus();
+			break;
+		}
+		else if (!currItem.isStyle(DwtMenuItem.SEPARATOR_STYLE) && currItem.getEnabled() && currItem.getVisible()) {
+			break;
+		}
 		currItem = (which === false) ? this._children.getPrev(currItem) : this._children.getNext(currItem);
 	}
 	if (!currItem) { return; }
@@ -904,10 +913,10 @@ function(which) {
 	var mev = new DwtMouseEvent();
 	if (this.__currentItem) {
 		this._setMouseEvent(mev, {dwtObj:this.__currentItem});
-		this.__currentItem.notifyListeners(DwtEvent.ONMOUSEOUT, mev);
+		this.__currentItem.notifyListeners(AjxEnv.isIE ? DwtEvent.ONMOUSELEAVE : DwtEvent.ONMOUSEOUT, mev);
 	}
 	this._setMouseEvent(mev, {dwtObj:currItem});
-	currItem.notifyListeners(DwtEvent.ONMOUSEOVER, mev);	// mouseover selects a menu item
+	currItem.notifyListeners(AjxEnv.isIE ? DwtEvent.ONMOUSEENTER : DwtEvent.ONMOUSEOVER, mev);	// mouseover selects a menu item
 	this.__currentItem = currItem;
 	this.scrollToItem(currItem, true);
 };
@@ -1154,7 +1163,7 @@ function(incScroll) {
 };
 
 DwtMenu.prototype._doPopdown =
-function() {
+function(ev) {
 	// Notify all sub menus to pop themselves down
 	var a = this._children.getArray();
 	var s = this._children.size();
@@ -1165,7 +1174,8 @@ function() {
 	}
 	this.setZIndex(Dwt.Z_HIDDEN);
 	this.setLocation(Dwt.LOC_NOWHERE, Dwt.LOC_NOWHERE);
-	
+	this._ev = ev;
+
 	this.notifyListeners(DwtEvent.POPDOWN, this);
 
 	var omem = DwtOutsideMouseEventMgr.INSTANCE;
@@ -1259,7 +1269,7 @@ function(ev) {
 
 		// If we've gotten here, the mousedown happened outside the active
 		// menu, so we hide it.
-		menu.popdown();
+		menu.popdown(0, ev);
 		
 		//it should remove all the active menus 
 		var cMenu = null ;
@@ -1287,8 +1297,8 @@ function() {
 };
 
 DwtMenu.closeActiveMenu =
-function() {
+function(ev) {
 	if (DwtMenu._activeMenuUp) {
-		DwtMenu._activeMenu.popdown();
+		DwtMenu._activeMenu.popdown(0, ev);
 	}
 };
