bef360e3148815763b3056add5b2a605c3cb974e
Unknown Perforce User <dcomfort>
2011-08-26 17:41:41 -0500

    Bug: 63878 - Need unique id for the "label" in options for "Change Password" Integrate from HELIX. Add id to element
    http://bugzilla.zimbra.com/show_bug.cgi?id=63878
    
    Copied from Perforce
     Change: 327701

--- WebRoot/js/ajax/dwt/xforms/XFormItem.js
+++ WebRoot/js/ajax/dwt/xforms/XFormItem.js
@@ -349,6 +349,8 @@ XFormItem.prototype.openSelectionLabel = "";
 // error handling
 XFormItem.prototype.errorLocation = _SELF_;
 
+// show help tooltip icon
+XFormItem.prototype.helpTooltip = false;
 //
 // Methods
 //
@@ -1056,11 +1058,25 @@ XFormItem.prototype.outputLabelCellHTML = function (html,  rowSpan, labelLocatio
 								"<label for='", this.getId(), "'>", label, "</label>"
 				);
 		}else{
-			html.append( "<td id=\"", this.getId(),"___label\"", 
-				this.getLabelCssString(), 
-				(rowSpan > 1 ? " rowspan=" + rowSpan : ""), ">", 
-				label
-			);
+            if(!this.getInheritedProperty("helpTooltip") ||
+               !this.getInheritedProperty("showHelpTooltip") ||
+               !this.getInheritedProperty("hideHelpTooltip") ){
+                html.append( "<td id=\"", this.getId(),"___label\"",
+                    this.getLabelCssString(),
+                    (rowSpan > 1 ? " rowspan=" + rowSpan : ""), ">",
+                    label
+                );
+            }else{
+                html.append( "<td id=\"", this.getId(),"___label\"",
+                    this.getLabelCssString(),
+                    " onclick=\"", "XFormItem.prototype.showHelpTooltip" ,
+			        ".call(" ,   this.getGlobalRef(), ", event );\" ",
+                    " onmouseout=\"", "XFormItem.prototype.hideHelpTooltip" ,
+			        ".call(" ,   this.getGlobalRef(), ", event );\" ",
+                    (rowSpan > 1 ? " rowspan=" + rowSpan : ""), ">",
+                    label
+                );
+            }
 		}
 		if (this.getRequired()) {
 			html.append("<span class='redAsteric'>*</span>");
@@ -1984,7 +2000,6 @@ function (event) {
 	var tooltip = shell.getToolTip();
 	tooltip.popdown();
 }
-
 
 
 
@@ -3450,7 +3465,11 @@ Repeat_XFormItem.prototype.getAddButton = function () {
 		var width = this.getInheritedProperty("addButtonWidth");		
 		if (width)
 			this.addButton.width = width ;
-			
+
+        var cssStyle = this.getInheritedProperty("addButtonCSSStyle");
+		if (cssStyle)
+			this.addButton.cssStyle = cssStyle ;
+
 		if(showAddOnNextRow) {
 			this.addButton.colSpan = "*";
 		}
@@ -4135,8 +4154,13 @@ Dwt_Adaptor_XFormItem.prototype._addCssStylesToDwtWidget = function () {
 Dwt_Button_XFormItem = function() {}
 XFormItemFactory.createItemType("_DWT_BUTTON_", "dwt_button", Dwt_Button_XFormItem, Dwt_Adaptor_XFormItem)
 Dwt_Button_XFormItem.estimateMyWidth = function (label,withIcon,extraMargin) {
-	var width = (String(label).length/2)*XForm.FONT_WIDTH1 + (String(label).length/2)*XForm.FONT_WIDTH2 + 14;
-	if(withIcon)
+    var width;
+    if(ZaZimbraAdmin.LOCALE=="ja"||ZaZimbraAdmin.LOCALE=="ko"||ZaZimbraAdmin.LOCALE=="zh_CN"||ZaZimbraAdmin.LOCALE=="zh_HK")
+         width = (String(label).length)*XForm.FONT_WIDTH1 + (String(label).length)*XForm.FONT_WIDTH2 + 14;
+    else
+	     width = (String(label).length/2)*XForm.FONT_WIDTH1 + (String(label).length/2)*XForm.FONT_WIDTH2 + 14;
+
+    if(withIcon)
 		width = width + 24;
 	
 	if(extraMargin>0)
@@ -4146,6 +4170,7 @@ Dwt_Button_XFormItem.estimateMyWidth = function (label,withIcon,extraMargin) {
 //	type defaults
 Dwt_Button_XFormItem.prototype.labelLocation = DwtLabel.IMAGE_LEFT | DwtLabel.ALIGN_CENTER;
 Dwt_Button_XFormItem.prototype.writeElementDiv = false;
+Dwt_Button_XFormItem.prototype.autoPadding= true;
 //	methods
 
 Dwt_Button_XFormItem.prototype.insertWidget = function (form, widget, element) {
@@ -4206,10 +4231,11 @@ Dwt_Button_XFormItem.prototype.constructWidget = function () {
 			}
 			 
 			el =  widget.getHtmlElement();
-                        var tableEl = el.firstChild;
-                       	if(!tableEl.style.width){
-                        	tableEl.style.width = "100%";
-                        }
+            var tableEl = el.firstChild;
+            var isAutoPadding = this.getInheritedProperty("autoPadding");
+            if(!tableEl.style.width && isAutoPadding){
+                 tableEl.style.width = "100%";
+            }
 
 		}		
 	}catch(ex){
@@ -4526,7 +4552,7 @@ Dwt_List_XFormItem.prototype.constructWidget = function () {
 			widget.setSize(width, height);
 		
 		//set the listDiv height
-		if (height) {
+		if (height && height != Dwt.DEFAULT) {
 			widget.setListDivHeight (height) ;
 		}
 	}		
