ca9b8f17397261a43d9bde5ac4746d7f36514602
Vince B. Bellows <vbellows@zimbra.com>
2014-11-19 10:11:13 -0600

    Bug: 96459 -  IE: View menu does not display submenus on mouseover
    Merging
    //depot/zimbra/JUDASPRIEST/ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtMenuItem.js to 
    //depot/zimbra/main/ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtMenuItem.js
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=96459
    
    Copied from Perforce
     Change: 529375

--- WebRoot/js/ajax/dwt/widgets/DwtMenuItem.js
+++ WebRoot/js/ajax/dwt/widgets/DwtMenuItem.js
@@ -422,10 +422,13 @@ function(ev) {
 	var menu = menuItem.parent;
 	if (menuItem.isSeparator()) { return false; }
 	DwtButton._mouseOverListener(ev, menuItem);
-	menu._popdownSubmenus();
+	if (!ev.ersatz) {
+		menu._popdownSubmenus();
+	}
 	if (!menuItem.hasFocus() && menuItem.getEnabled()) {
 		menuItem.focus();
 	}
+
 	if (menuItem._menu && !ev.ersatz) {
 		menuItem._popupMenu(menuItem._hoverDelay);
 	}
