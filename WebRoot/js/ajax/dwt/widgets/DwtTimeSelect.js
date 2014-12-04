8058d67f3ae86d4f7f23c8078062b53f7cfd2eaa
Tony Publiski <tpubliski@zimbra.com>
2014-12-03 20:43:23 -0600

    Integrated the following from //depot/zimbra/JUDASPRIEST/...: Change 530904 by vbellows@vbellows-JP on 2014/12/03 18:28:48
    Bug: 96742 -  Time selection defaults to midnight
    
    
    Accessibility changes in DwtMenu and DwtButton were resetting the selected item to the first item in the menu.  For time selection, I've moved the scrolling (to position to the current time value) to be after DwtButton.popup, which is where the menu.setSelectedItem(0) calls occur.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=96742
    
    Copied from Perforce
     Change: 530918

--- WebRoot/js/ajax/dwt/widgets/DwtTimeSelect.js
+++ WebRoot/js/ajax/dwt/widgets/DwtTimeSelect.js
@@ -667,6 +667,8 @@ function(enabled) {
 
 DwtTimeInput.prototype._timeButtonListener =
 function(ev) {
+	var timeFormatter = AjxDateFormat.getTimeInstance(AjxDateFormat.SHORT);
+	var defaultTopMenuItem;
     if(!this._menuItemsAdded) {
         var j,
             k,
@@ -677,9 +679,7 @@ function(ev) {
             minutesSelectMenu,
             now = new Date(),
             timeSelectButton = this._timeSelectBtn,
-            timeFormatter = AjxDateFormat.getTimeInstance(AjxDateFormat.SHORT),
-            menuSelectionListener = new AjxListener(this, this._timeSelectionListener),
-            defaultTopMenuItem;
+            menuSelectionListener = new AjxListener(this, this._timeSelectionListener);
 
         for (j = 0; j < 24; j++) {
             now.setHours(j);
@@ -713,12 +713,13 @@ function(ev) {
         }
         this._hoursSelectMenu.setWidth(timeSelectButton.getW() + this._timeSelectInput.getW());
 
-	if (defaultTopMenuItem)
-		this._hoursSelectMenu.scrollToItem(defaultTopMenuItem);
-        this._scrollToValue(timeFormatter.format(this.getValue()));
         this._menuItemsAdded = true;
     }
 	ev.item.popup();
+	if (defaultTopMenuItem) {
+		this._hoursSelectMenu.scrollToItem(defaultTopMenuItem);
+	}
+	this._scrollToValue(timeFormatter.format(this.getValue()));
 };
 
 DwtTimeInput.prototype._timeSelectionListener =
