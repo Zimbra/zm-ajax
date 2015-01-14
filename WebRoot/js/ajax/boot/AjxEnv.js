4de10e7704b6a9d45dae7b5a72350269c66ac468
Dan Christiansen (c) <dchristiansen@zimbra.com>
2015-01-14 02:56:06 -0600

    bug 97247 - fix AJAX UI in IE8
    Changes made in bug 96999, comment 1 and bug 96792, comment 10 added
    calls to 'Object.keys()'. This method is unavailable in IE8, however,
    causing the AJAX UI to not load in that browser.
    
    We fix this by adding a polyfill, extracted from the Mozilla Developer
    Network.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97247
    
    Copied from Perforce
     Change: 533907

--- WebRoot/js/ajax/boot/AjxEnv.js
+++ WebRoot/js/ajax/boot/AjxEnv.js
@@ -590,3 +590,50 @@ if (typeof Object.getPrototypeOf !== "function") {
 		};
 	}
 }
+
+/**
+ * Polyfill for Object.keys -- currently only required by IE8.
+ *
+ * From Mozilla:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+ */
+if (typeof Object.keys !== "function") {
+	Object.keys = (function() {
+		'use strict';
+		var hasOwnProperty = Object.prototype.hasOwnProperty,
+			hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+			dontEnums = [
+				'toString',
+				'toLocaleString',
+				'valueOf',
+				'hasOwnProperty',
+				'isPrototypeOf',
+				'propertyIsEnumerable',
+				'constructor'
+			],
+			dontEnumsLength = dontEnums.length;
+
+		return function(obj) {
+			if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+				throw new TypeError('Object.keys called on non-object');
+			}
+
+			var result = [], prop, i;
+
+			for (prop in obj) {
+				if (hasOwnProperty.call(obj, prop)) {
+					result.push(prop);
+				}
+			}
+
+			if (hasDontEnumBug) {
+				for (i = 0; i < dontEnumsLength; i++) {
+					if (hasOwnProperty.call(obj, dontEnums[i])) {
+						result.push(dontEnums[i]);
+					}
+				}
+			}
+			return result;
+		};
+	}());
+}
