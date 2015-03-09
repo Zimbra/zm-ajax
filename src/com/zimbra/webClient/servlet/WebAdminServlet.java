a615615274411ce51fda36f984e22deec5739178
Vince B. Bellows <vbellows@zimbra.com>
2015-03-09 11:22:36 -0500

    Bug: 98089 - Expanding DL address and pressing Down(↓) arrow from keyboard it opens context menu list. Back out the change made to DwtKeyboardMgr - its causing the attendee field in FF to never get focus.  Pulling it until I figure out what to do with it. Merging //depot/zimbra/JUDASPRIEST/ZimbraWebClient/WebRoot/js/ajax/dwt/keyboard/DwtKeyboardMgr.js to //depot/zimbra/main/ZimbraWebClient/WebRoot/js/ajax/dwt/keyboard/DwtKeyboardMgr.js
    https://bugzilla.zimbra.com/show_bug.cgi?id=98089
    
    Copied from Perforce
     Change: 539038

---
+++ src/com/zimbra/webClient/servlet/WebAdminServlet.java
@@ -0,0 +1,103 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2014 Zimbra, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.webClient.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.Server;
+import com.zimbra.cs.consul.CatalogRegistration;
+import com.zimbra.cs.consul.ConsulClient;
+import com.zimbra.cs.consul.ConsulServiceLocator;
+import com.zimbra.cs.consul.ServiceLocator;
+
+
+/**
+ * The WebServlet and WebAdminServlet servlets are used as a central place to hook
+ * lifecycle events for these two webapps. This creates a place to perform Service Locator integration.
+ */
+@SuppressWarnings("serial")
+public class WebAdminServlet extends HttpServlet {
+    protected ServiceLocator serviceLocator;
+    protected String serviceID;
+
+    public WebAdminServlet() {
+        super ();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ConsulClient consulClient = new ConsulClient();
+        serviceLocator = new ConsulServiceLocator(consulClient);
+        registerWithServiceLocator();
+    }
+
+    @Override
+    public void destroy() {
+        deregisterWithServiceLocator();
+        super.destroy();
+    }
+
+    /**
+     * Register with service locator.
+     *
+     * @see https://www.consul.io/docs/agent/http.html#_v1_catalog_register
+     */
+    protected void registerWithServiceLocator() throws ServletException {
+
+        try {
+            // Read protocol and port configuration
+            Server localServer = Provisioning.getInstance().getLocalServer();
+            String schemePrefix = localServer.getAdminServiceScheme();
+            int httpsPort = localServer.getAdminPort();
+
+            // Register https endpoint
+            if ("https://".equals(schemePrefix)) {
+                serviceID = registerWithServiceLocator("zimbra:webadminssl", httpsPort, "https");
+            }
+        } catch (ServiceException e) {
+            throw new ServletException("Failed reading provisioning config before registering with service locator", e);
+        }
+    }
+
+    protected String registerWithServiceLocator(String serviceName, int port, String checkScheme) {
+        String serviceID = serviceName + ":" + port;
+        CatalogRegistration.Service service = new CatalogRegistration.Service(serviceID, serviceName, port);
+        String url = checkScheme + "://localhost:" + port + "/zimbraAdmin/";
+        CatalogRegistration.Check check = new CatalogRegistration.Check(serviceID + ":health", serviceName);
+        check.script = "/opt/zimbra/libexec/zmhealthcheck-webadmin " + url;
+        check.interval = "1m";
+        service.check = check;
+        serviceLocator.registerSilent(service);
+        return serviceID;
+    }
+
+    /**
+     * De-register with service locator.
+     *
+     * @see https://www.consul.io/docs/agent/http.html#_v1_catalog_deregister
+     */
+    protected void deregisterWithServiceLocator() {
+        if (serviceID != null) {
+            serviceLocator.deregisterSilent(serviceID);
+            serviceID = null;
+        }
+    }
+}
