92f34cf2ae1688f049d3f05dfcec5f09634ff20e
Gren Elliot <gren.elliot@zimbra.com>
2016-03-07 03:44:58 -0600

    Bug:103995
    Block direct access to .swf files under /yui/
    Charts under Admin Console's Monitor / Advanced Statistics and under Monitor / Server Statistics (select server) / Disk / Last 48 hours still function with this block.
    
    Firefox browse to these URLs (with appropriate server names) will now yield 403 errors
    https://example.com/yui/2.7.0/charts/assets/charts.swf?allowedDomain=\%22}%29%29%29}catch%28e%29{alert%28%27XSS%27%29}//
    https://example.com:7071/zimbraAdmin/yui/2.7.0/charts/assets/charts.swf?allowedDomain=\%22})))}catch(e){alert(%27XSS%27)}//
    https://reviewboard.eng.zimbra.com/r/7340/
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=103995
    
    Copied from Perforce
     Change: 567626

---
+++ src/com/zimbra/webClient/filters/YuiSwfFilter.java
@@ -0,0 +1,56 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2016 Zimbra, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.webClient.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.zimbra.common.util.ZimbraLog;
+
+public class YuiSwfFilter implements Filter {
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+                    ServletException {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpReq = (HttpServletRequest) request;
+            String requestURI = httpReq.getRequestURI();
+            if (requestURI.endsWith(".swf")) {
+                ZimbraLog.misc.debug("Rejecting request for direct access to .swf file %s", requestURI);
+                ((HttpServletResponse)response).sendError(HttpServletResponse.SC_FORBIDDEN);
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+}
