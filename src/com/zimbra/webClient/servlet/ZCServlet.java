176c6597ca798f5b5a577be959f41dd6ebb05e3b
Gren Elliot <gren.elliot@zimbra.com>
2016-03-08 08:14:36 -0600

    Bug:103995
    Block direct access to .swf files under /yui/ if any query string has been provided.
    https://reviewboard.eng.zimbra.com/r/7346/
    JUDASPRIEST ---> main
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=103995
    
    Copied from Perforce
     Change: 567736

--- src/com/zimbra/webClient/servlet/ZCServlet.java
+++ src/com/zimbra/webClient/servlet/ZCServlet.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
  * Copyright (C) 2005, 2006, 2007, 2009, 2010, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -25,7 +25,7 @@ import javax.naming.*;
 
 public class ZCServlet extends HttpServlet
 {
-    
+
     private static final String PARAM_AUTH_TOKEN = "authToken";
     private static final String PARAM_QUERY_STRING_TO_CARRY = "qs";
     private static final String PARAM_AUTH_TOKEN_LIFETIME = "atl";
@@ -34,14 +34,14 @@ public class ZCServlet extends HttpServlet
     private static String httpsPort;
     private static String httpPort;
     private static String protocolMode;
-    private static final String DEFAULT_HTTPS_PORT = "443";
-    private static final String DEFAULT_HTTP_PORT = "80";
-    private static final String PROTO_MIXED = "mixed";
-    private static final String PROTO_HTTP = "http";
-    private static final String PROTO_HTTPS = "https";
-    
-    
-    static 
+    public static final String DEFAULT_HTTPS_PORT = "443";
+    public static final String DEFAULT_HTTP_PORT = "80";
+    public static final String PROTO_MIXED = "mixed";
+    public static final String PROTO_HTTP = "http";
+    public static final String PROTO_HTTPS = "https";
+
+
+    static
     {
         try {
             Context initCtx = new InitialContext();
@@ -51,15 +51,15 @@ public class ZCServlet extends HttpServlet
             if (httpsPort != null && httpsPort.equals(DEFAULT_HTTP_PORT)){
                 httpsPort = "";
             } else {
-                httpsPort = ":" + httpsPort;    
+                httpsPort = ":" + httpsPort;
             }
             httpPort = (String) envCtx.lookup("httpPort");
             if (httpPort != null && httpPort.equals(DEFAULT_HTTP_PORT)){
                 httpPort = "";
             } else {
-                httpPort = ":" + httpPort;    
+                httpPort = ":" + httpPort;
             }
-            
+
         } catch (NamingException ne) {
             protocolMode = PROTO_HTTP;
             httpsPort = "";
@@ -67,13 +67,13 @@ public class ZCServlet extends HttpServlet
         }
     }
 
-    public void doGet (HttpServletRequest req, HttpServletResponse resp) 
+    public void doGet (HttpServletRequest req, HttpServletResponse resp)
         throws ServletException, java.io.IOException
     {
         doPost(req, resp);
     }
-    
-    public void doPost (HttpServletRequest req, HttpServletResponse resp) 
+
+    public void doPost (HttpServletRequest req, HttpServletResponse resp)
         throws ServletException, java.io.IOException
     {
         return;
@@ -85,13 +85,13 @@ public class ZCServlet extends HttpServlet
         boolean ret = false;
         String initMode = getReqParameter(req, "initMode");
         if ( !protocolMode.equals(proto) && !(protocolMode.equals(PROTO_MIXED)
-                                              &&( (initMode == null) || 
+                                              &&( (initMode == null) ||
                                                   initMode.equals(proto))) ) {
             ret = true;
         }
         return ret;
     }
-    
+
 
     /**
      * getRedirectUrl assumes that the caller needs to redirect based
@@ -104,7 +104,7 @@ public class ZCServlet extends HttpServlet
     {
         return getRedirectUrl(req, null, null, true, false);
     }
-    protected String getRedirectUrl( HttpServletRequest req, String uri, 
+    protected String getRedirectUrl( HttpServletRequest req, String uri,
                                      String queryString, boolean absolute,
                                      boolean removeInitMode)
     {
@@ -121,7 +121,7 @@ public class ZCServlet extends HttpServlet
                         }
                         tempQs.append(pairs[i]);
                     }
-                }   
+                }
             }
         }
         if (tempQs.length() > 0 ){
@@ -129,9 +129,9 @@ public class ZCServlet extends HttpServlet
         } else {
             qs = "";
         }
-        
+
         //System.out.println("absolute? = " + absolute);
-        if (absolute) {       
+        if (absolute) {
             String proto = req.getScheme();
             int port = req.getServerPort();
             String portString = port == 80? "" : ":" + port;
@@ -172,7 +172,7 @@ public class ZCServlet extends HttpServlet
             buf.append(req.getServerName());
             buf.append(portString);
         }
-        
+
         if (uri != null){
             buf.append(uri);
         } else {
@@ -189,7 +189,7 @@ public class ZCServlet extends HttpServlet
         }
         return buf.toString();
     }
-    
+
     protected String getReqParameter(HttpServletRequest req, String paramName,
                                      String defaultValue)
     {
@@ -204,6 +204,6 @@ public class ZCServlet extends HttpServlet
     {
         return getReqParameter(req, paramName, null);
     }
-    
-	
+
+
 }
