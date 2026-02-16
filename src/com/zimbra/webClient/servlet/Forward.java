bf1a51f209322fc86fe74f4fe6956279da236009
omkar.oak <omkar.oak@synacor.com>
2026-02-16 19:24:39 +0530

    10.1.17 ZBUG-5302 : Unauthenticated LFI via fu parameter allows access to /WEB-INF/web.xml

--- src/com/zimbra/webClient/servlet/Forward.java
+++ src/com/zimbra/webClient/servlet/Forward.java
@@ -17,35 +17,95 @@
 
 package com.zimbra.webClient.servlet;
 
+import com.zimbra.common.localconfig.LC;
+import com.zimbra.common.util.ZimbraLog;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.zimbra.common.util.ZimbraLog;
-
 @SuppressWarnings("serial")
-public class Forward extends ZCServlet
-{
-    public static final String DEFAULT_FORWARD_URL = 
-	"/public/login.jsp";
-    private static final String PARAM_FORWARD_URL = "fu";
-    
-    public void doGet (HttpServletRequest req,
-		       HttpServletResponse resp) {
+public class Forward extends ZCServlet {
+    public static final String DEFAULT_FORWARD_URL = "/public/login.jsp";
 
-	try {
-	    String url = getReqParameter(req, PARAM_FORWARD_URL,
-                                         DEFAULT_FORWARD_URL);
-	    String qs = req.getQueryString();
-	    if (qs != null && !qs.equals("")){
-		url = url + "?" + qs;
-	    }
-	    ServletContext sc = getServletConfig().getServletContext();
-	    sc.getRequestDispatcher(url).forward(req, resp);
-	} catch (Exception ex) {
-		ZimbraLog.webclient.warn("exception forwarding", ex);
-		if (!resp.isCommitted())
-			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-	}
-    }    
+    private static final String PARAM_FORWARD_URL = "fu";
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+        try {
+            String url = getReqParameter(req, PARAM_FORWARD_URL, DEFAULT_FORWARD_URL);
+            String qs = req.getQueryString();
+            if (qs != null && !qs.equals("")) {
+                url = url + "?" + qs;
+            }
+            ServletContext sc = getServletConfig().getServletContext();
+            sc.getRequestDispatcher(url).forward(req, resp);
+        } catch (SecurityException ex) {
+            ZimbraLog.webclient.warn("fu param did not match zmlocalconfig zimbra_forward_servlet_allowed_paths", ex);
+            if (!resp.isCommitted()) {
+                resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            }
+        } catch (Exception ex) {
+            ZimbraLog.webclient.warn("exception forwarding", ex);
+            if (!resp.isCommitted()) {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
+
+    @Override
+    protected String getReqParameter(HttpServletRequest req, String paramName, String defaultValue) {
+
+        String val = req.getParameter(paramName);
+
+        if (val == null || val.isEmpty()) {
+            return defaultValue;
+        }
+
+        if (PARAM_FORWARD_URL.equals(paramName) && LC.zimbra_forward_servlet_url_validation_enabled.booleanValue()
+                && !isAllowedForwardUrl(val)) {
+            throw new SecurityException("Forbidden forward URL: " + val);
+        }
+
+        return val;
+    }
+
+    private boolean isAllowedForwardUrl(String url) {
+        if (url.length() > 2000) {
+            return false;
+        }
+        if (containsBaselineRestrictedToken(url)) {
+            return false;
+        }
+        // after decoding url validate exact matches from allowed paths
+        String[] paths = getAllowedUrls().split(",");
+        for (String p : paths) {
+            if (url.equals(p.trim())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getAllowedUrls() {
+        return LC.zimbra_forward_servlet_allowed_paths.value();
+    }
+
+    private boolean containsBaselineRestrictedToken(String value) {
+        try {
+            String decoded = URLDecoder.decode(value, StandardCharsets.UTF_8.name());
+            decoded = decoded.toLowerCase();
+            if (decoded.contains("..")) {
+                return true;
+            }
+            if (decoded.contains("\\")) {
+                return true;
+            }
+            return !decoded.startsWith("/");
+        } catch (UnsupportedEncodingException e) {
+            return true; // malformed encoding = reject
+        }
+    }
 }
